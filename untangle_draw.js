e854ead0115eb57c5517c1f16e8027523c4a54b5
steffleupagus <72820144+steffleupagus@users.noreply.github.com>
2026-01-27 21:24:04 -0600

    Add files via upload

--- untangle_draw.js
+++ untangle_draw.js
@@ -14,7 +14,8 @@ function drawLine(ctx, x1, y1, x2, y2, color1, color2, thickness, label="", avoi
 	ctx.fillStyle = domain_gradient;
 	ctx.strokeStyle = domain_gradient;
 	
-    drawArrow(ctx,x1,y1,x2,y2,3,1,Math.PI/12,30, avoidPoints);
+    //drawArrow(ctx,x1,y1,x2,y2,3,1,Math.PI/12,30, avoidPoints);
+	drawArrow(ctx,x1,y1,x2,y2,3,0,Math.PI/12,30, avoidPoints);
 	
 	if (untangleGame.flags["Labels"])
 	{
@@ -143,9 +144,7 @@ var drawArrow=function(ctx,x1,y1,x2,y2,style,which,angle,d, avoidPoints)
 	var lineangle=Math.atan2(y2-y1,x2-x1);
 	// h is the line length of a side of the arrow head
 	var h=Math.abs(d/Math.cos(angle));
-		
-	return;
-	
+			
 	if(which&1)
 	{	// handle far end arrow head
 		var angle1=lineangle+Math.PI+angle;
